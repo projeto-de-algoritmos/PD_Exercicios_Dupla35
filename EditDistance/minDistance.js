da6f7328e70ca0e20ea1664f091acd72190cf71e
rafaelxavierr <rafael@DESKTOP-E7AJREI.localdomain>
2023-07-10 18:40:35 -0300

    Adicionando comentários à solução

--- EditDistance/minDistance.js
+++ EditDistance/minDistance.js
@@ -4,23 +4,26 @@ var minDistance = function(word1, word2) {
   
     const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
 
+    // Inicializar a primeira coluna
     for (let i = 0; i <= m; i++) {
         dp[i][0] = i;
     }
     
+    // Inicializar a primeira linha
     for (let j = 0; j <= n; j++) {
         dp[0][j] = j;
     }
 
+    // Preencher a matriz dp
     for (let i = 1; i <= m; i++) {
         for (let j = 1; j <= n; j++) {
             if (word1[i - 1] === word2[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1];
             } else {
                 dp[i][j] = Math.min(
-                dp[i][j - 1] + 1,       
-                dp[i - 1][j] + 1,       
-                dp[i - 1][j - 1] + 1    
+                dp[i][j - 1] + 1,       // Inserção
+                dp[i - 1][j] + 1,       // Remoção
+                dp[i - 1][j - 1] + 1    // Substituição
                 );
             }
         }
